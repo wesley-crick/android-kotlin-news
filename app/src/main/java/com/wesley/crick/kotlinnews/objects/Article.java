8fb5a1b164f348274006e8e4d69205bc81e0a0b2
Wesley Crick <wesley@microspec.com>
2020-09-29 19:05:40 -0400

    Create Article
    Basic class attribute that will be used when pulling articles.

---
+++ app/src/main/java/com/wesley/crick/kotlinnews/objects/Article.java
@@ -0,0 +1,10 @@
+package com.wesley.crick.kotlinnews.objects;
+
+import org.json.JSONObject;
+
+public class Article {
+    private String title;
+    private String thumbnail;
+    private String selftext_html;
+    private String url;
+}

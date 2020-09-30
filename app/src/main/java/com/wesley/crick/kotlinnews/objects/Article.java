10179faa2f5f51c5c4cbf761503491b9b9364645
Wesley Crick <wesley@microspec.com>
2020-09-30 15:54:24 -0400

    Add new fields to article. Changed app name

--- app/src/main/java/com/wesley/crick/kotlinnews/objects/Article.java
+++ app/src/main/java/com/wesley/crick/kotlinnews/objects/Article.java
@@ -1,20 +1,22 @@
 package com.wesley.crick.kotlinnews.objects;
 
+import androidx.annotation.NonNull;
+
 import org.json.JSONObject;
 
 public class Article {
     private String title;
     private String thumbnail;
-    private String html;    //selftext_html
-    private String url;
+    private String html;    //The html used to describe the post ["selftext_html"]
+    private String overrideUrl; //Link to external site or image ["url_overridden_by_dest"]
+    private ArticleType articleType;
 
     private boolean hasThumbnail;
 
-    public Article(String title, String thumbnail, String html, String url) {
+    public Article(String title, String thumbnail, String html) {
         this.title = title;
         this.thumbnail = thumbnail;
         this.html = html;
-        this.url = url;
 
         validate();
     }
@@ -23,7 +25,19 @@ public class Article {
         this.title = obj.optString("title", "");
         this.thumbnail = obj.optString("thumbnail", "");
         this.html = obj.optString("selftext_html", "");
-        this.url = obj.optString("url", "");
+        this.overrideUrl = obj.optString("url_overridden_by_dest", "");
+
+        String postType = obj.optString("post_hint", "");
+        switch(postType) {
+            case "image":
+                this.articleType = ArticleType.image;
+                break;
+            default:
+                this.articleType = ArticleType.link;
+        }
+        if ( this.html != "" ) {
+            this.articleType = ArticleType.text;
+        }
 
         validate();
     }
@@ -34,9 +48,7 @@ public class Article {
             this.thumbnail = "";
         }
 
-        if ( this.thumbnail == null ) {
-            hasThumbnail = false;
-        } else hasThumbnail = !this.thumbnail.isEmpty();
+        hasThumbnail = !this.thumbnail.isEmpty();
 
         // Todo validate some fields
     }
@@ -49,15 +61,24 @@ public class Article {
         return thumbnail;
     }
 
-    public String getHTML() {
+    public String getHtml() {
         return html;
-    }
-
-    public String getUrl() {
-        return url;
     }
 
     public boolean hasThumbnail() {
         return hasThumbnail;
     }
+
+    public ArticleType getArticleType() {
+        return articleType;
+    }
+
+    public String getOverrideUrl() {
+        return overrideUrl;
+    }
+
+    public enum ArticleType {
+        image, link, text
+    }
+
 }
